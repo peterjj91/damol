d63ad886f50372152ad4f568d2074b5ed61237de
ppopeskul <pasha.9191@mail.ru>
2018-09-04 14:46:13 +0300

    Finish

--- src/scripts/z_main.js
+++ src/scripts/z_main.js
@@ -73,6 +73,69 @@ $('.promo-slider').slick({
     ]
 });
 
+// product slider
+$('.product-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: false,
+    arrows: true,
+    fade: true,
+    speed: 300,
+    lazyLoad: 'ondemand',
+    asNavFor: '.product-nav',
+    prevArrow: '<div class="slick-prev"><i class="i-chev-left-thin"></i><span class="sr-text">Previous</span></div>',
+    nextArrow: '<div class="slick-next"><i class="i-chev-right-thin"></i><span class="sr-text">Next</span></div>'
+});
+
+$('.product-nav').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    infinite: false,
+    centerPadding: '0px',
+    asNavFor: '.product-for',
+    dots: false,
+    // centerMode: true,
+    draggable: true,
+    speed: 200,
+    focusOnSelect: true,
+    prevArrow: '<div class="slick-prev"><i class="i-chev-left-thin"></i><span class="sr-text">Previous</span></div>',
+    nextArrow: '<div class="slick-next"><i class="i-chev-right-thin"></i><span class="sr-text">Next</span></div>',
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+
+            }
+        }
+    ]
+});
+
+// slider company
+$('.slider-company').slick({
+    slidesToShow: 5,
+    arrows: true,
+    slidesToScroll: 1,
+    infinite: false,
+    centerPadding: '0px',
+    dots: false,
+    speed: 200,
+    focusOnSelect: true,
+    responsive: [
+        {
+            breakpoint: 1050,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 700,
+            settings: {
+                slidesToShow: 2,
+            }
+        }
+    ]
+});
+
 // datepicker
 $(document).ready(function() {
     $('.calendar-item').datepicker({
